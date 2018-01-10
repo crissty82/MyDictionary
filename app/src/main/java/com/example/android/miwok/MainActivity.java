1134515ca20cf77bab4d78e7a6394c35362fc10e
Cristina <crissty82@yahoo.com>
2018-01-10 12:50:55 +0200

    Changed activity_numbers.xml file to a LinearLayout root-view. Added 3 numbers to the NumbersActivity and listed them as child-views of type text-view in activity_numbers root-view

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -15,12 +15,14 @@
  */
 package com.example.android.miwok;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -31,12 +33,65 @@ public class MainActivity extends AppCompatActivity {
         // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
 
-        NumbersClickListener clickListener = new NumbersClickListener();
-
         //Find the view that shows the numbers category
         TextView numbers = (TextView) findViewById(R.id.numbers);
 
         //Set a clickListener to the view
-        numbers.setOnClickListener(clickListener);
+        numbers.setOnClickListener(new OnClickListener(){
+            @Override
+            public void onClick(View view){
+
+                //Toast.makeText(view.getContext(), "Open the list of numbers", Toast.LENGTH_SHORT).show();
+
+                Intent numbersIntent = new Intent(MainActivity.this, NumbersActivity.class);
+                startActivity(numbersIntent);
+            }
+        });
+
+        //Find the view that shows the family members category
+        TextView family = (TextView) findViewById(R.id.family);
+
+        //Set a clickListener to the view family
+        family.setOnClickListener(new OnClickListener(){
+            @Override
+            public void onClick(View view){
+
+                //Toast.makeText(view.getContext(), "Open the list of numbers", Toast.LENGTH_SHORT).show();
+
+                Intent familyIntent = new Intent(MainActivity.this, FamilyActivity.class);
+                startActivity(familyIntent);
+            }
+        });
+
+        //Find the view that shows the colors category
+        TextView colors = (TextView) findViewById(R.id.colors);
+
+        //Set a clickListener to the view colors
+        colors.setOnClickListener(new OnClickListener(){
+            @Override
+            public void onClick(View view){
+
+                //Toast.makeText(view.getContext(), "Open the list of numbers", Toast.LENGTH_SHORT).show();
+
+                Intent colorsIntent = new Intent(MainActivity.this, ColorsActivity.class);
+                startActivity(colorsIntent);
+            }
+        });
+
+        //Find the view that shows the phrases category
+        TextView phrases = (TextView) findViewById(R.id.phrases);
+
+        //Set a clickListener to the view phrases
+        phrases.setOnClickListener(new OnClickListener(){
+            @Override
+            public void onClick(View view){
+
+                //Toast.makeText(view.getContext(), "Open the list of numbers", Toast.LENGTH_SHORT).show();
+
+                Intent phrasesIntent = new Intent(MainActivity.this, PhrasesActivity.class);
+                startActivity(phrasesIntent);
+            }
+        });
+
     }
 }
